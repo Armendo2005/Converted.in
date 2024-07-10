5dfd4331a5ca1e15cdb907fa3ca2dd97f1d7e003
Armendo <Armendo2005@gmail.com>
2024-07-10 12:35:07 +0300

    start-project-skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsListComponent } from "./pages/products-list/products-list.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsListComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/pages/products-list/products-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-products-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.scss'
+})
+export class ProductsListComponent implements OnInit {
+  products: any[] = [];
+  searchKeyword: string = '';
+  categoryFilter: string = '';
+  priceRangeFilter: { min: number, max: number } = { min: 0, max: 10000 };
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    const apiUrl = 'https://dummyjson.com/products?limit=100';
+    this.http.get<any>(apiUrl).subscribe(data => {
+      this.products = data.products;
+      console.log(this.products);
+    });
+  }
+
+  applyFilters(): void {
+    // Apply filters based on search keyword, category, and price range
+    // Use this.searchKeyword, this.categoryFilter, and this.priceRangeFilter to filter the products array
+  }
+
+  paginateProducts(page: number): void {
+    this.currentPage = page;
+    // Perform pagination logic to display correct set of products based on the current page and items per page
+  }
+}
