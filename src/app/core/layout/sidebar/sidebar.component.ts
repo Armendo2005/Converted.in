ce4ab6f9ae46c9a16b24e0f2f39bc89fb58a3e54
Armendo <Armendo2005@gmail.com>
2024-07-10 23:13:17 +0300

    display Categories List in component

--- src/app/core/layout/sidebar/sidebar.component.ts
+++ src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CategoriesListComponent } from "../../../shared/component/categories-list/categories-list.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CategoriesListComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
--- src/app/shared/component/categories-list/categories-list.component.ts
+++ src/app/shared/component/categories-list/categories-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Router } from '@angular/router';
+import { ApiService } from '../../servcies/api.service';
 
 @Component({
   selector: 'app-categories-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './categories-list.component.html',
   styleUrl: './categories-list.component.scss'
 })
-export class CategoriesListComponent {
+export class CategoriesListComponent implements OnInit {
 
+  Categories = [];
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+   
+  ) {}
+
+  ngOnInit(): void {
+    
+    this.apiService.getCategoriesList().subscribe((categories) => {
+     this.Categories = categories;
+     console.log(this.Categories);
+   });
+  }
 }
