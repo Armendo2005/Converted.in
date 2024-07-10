d72b93e875ba7baf2568d2fb6caafb9d057558bf
Armendo <Armendo2005@gmail.com>
2024-07-10 20:28:19 +0300

    Add breadCrump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ProductsListComponent } from "./pages/products-list/products-list.component";
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from "./core/layout/header/header.component";
+import { BreadcrumpComponent } from "./core/layout/breadcrump/breadcrump.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductsListComponent, HttpClientModule, HeaderComponent],
+  imports: [RouterOutlet, ProductsListComponent, HttpClientModule, HeaderComponent, BreadcrumpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
